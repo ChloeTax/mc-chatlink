1e4528bb6925c7ba5ae82d5b977e3b006979b571
chloetax <overratedchloe@gmail.com>
2026-01-10 22:06:50 -0800

    Add 2-way discord support

--- chat_services/Discord.py
+++ chat_services/Discord.py
@@ -1,6 +1,11 @@
-import discord
-from discord_webhook import DiscordWebhook  # pyright: ignore[reportMissingTypeStubs]
+import json
+import threading
+import time
 
+import requests
+from discord_webhook import DiscordWebhook  # pyright: ignore[reportMissingTypeStubs]
+from websockets.sync.client import connect as ws_connect
+from websockets.sync.connection import Connection as ws_connection  # noqa: N813
 import chatlink
 
 
@@ -13,7 +18,10 @@ class DiscordService(chatlink.ChatService):
         discord_guild_id: int,
     ):
         self.webhook_url = webhook_url
-        # DiscordBot(discord_token, discord_channel_id, discord_guild_id)
+
+        discordbot = DiscordBot(discord_token, discord_channel_id, discord_guild_id)
+        discordbot.relay = self._relay
+        discordbot.start()
         super().__init__()
 
     def send(self, message: chatlink.Message):
@@ -30,16 +38,74 @@ class DiscordService(chatlink.ChatService):
         ).execute()
 
 
-class DiscordBot(discord.Client):
+class DiscordBot(threading.Thread):
     def __init__(
         self, discord_token: str, discord_channel_id: int, discord_guild_id: int
     ):
-        raise NotImplementedError
         self.discord_token = discord_token
         self.discord_channel_id = discord_channel_id
         self.discord_guild_id = discord_guild_id
 
-        intents = discord.Intents.default()
-        intents.message_content = True
-        intents.members = True
-        super().__init__(intents=intents)
+        self.ws = ws_connect(
+            requests.get(
+                "https://discord.com/api/v10/gateway/bot",
+                headers={"Authorization": f"Bot {self.discord_token}"},
+            ).json()["url"],
+            max_size=2147483648,
+        )
+
+        self.ws.send(
+            json.dumps({
+                "op": 2,
+                "d": {
+                    "token": self.discord_token,
+                    "intents": 512,
+                    "properties": {},
+                },
+            })
+        )
+
+        self.Heartbeat(
+            self.ws, json.loads(self.ws.recv())["d"]["heartbeat_interval"]
+        ).start()
+        super().__init__()
+
+    def relay(self, message: chatlink.Message):
+        pass
+
+    def run(self):
+        while True:
+            data = json.loads(self.ws.recv())
+            if (
+                data["t"] == "MESSAGE_CREATE"
+                and data["d"]["channel_id"] == str(self.discord_channel_id)
+                and "member" in data["d"]
+            ):
+                if name := data["d"]["member"]["nick"]:
+                    pass  # noqa: E701
+                elif name := data["d"]["author"]["global_name"]:
+                    pass  # noqa: E701
+                else:
+                    name = data["d"]["author"]["username"]  # noqa: E701
+
+                self.relay(
+                    message=chatlink.Message(
+                        author=chatlink.MessageAuthor(
+                            name=name,
+                            id=data["d"]["author"]["id"],
+                        ),
+                        content=chatlink.MessageContent(content=data["d"]["content"]),
+                        platform="Minecraft",
+                    )
+                )
+
+    class Heartbeat(threading.Thread):
+        def __init__(self, ws: ws_connection, interval: int):
+            self.ws = ws
+            self.interval = interval
+            super().__init__()
+
+        def run(self):
+            while True:
+                time.sleep(self.interval / 1000)
+                self.ws.send(json.dumps({"op": 1, "d": None}))
